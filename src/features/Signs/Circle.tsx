19270be460f3aa84c9361eec3b4dfa455c8c2de0
name <you@example.com>
2025-01-20 18:55:24 +0300

    Work check

--- src/features/Signs/Circle.tsx
+++ src/features/Signs/Circle.tsx
@@ -11,9 +11,9 @@ export default function Circle({size}: CircleProps) {
         case "large":
             sizeParam = 36
             break;
-            
-    }
 
+    }
+    
     return (
         <div className={`w-${sizeParam} h-${sizeParam} shrink-0 grow-0 rounded-full  border-green-400 border-solid border-4`}>
         </div>
